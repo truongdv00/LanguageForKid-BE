f8001046227236972bbe482ffa5389302193fecf
Sondq <sondq98@gmail.com>
2022-04-20 14:15:37 +0700

    push code rest api

---
+++ nodejs_api/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+dotenv.config()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+let routes = require('./api/routes') //importing route
+routes(app)
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+})
+
+app.listen(port)
+
+console.log('RESTful API server started on: ' + port)
