9a89d0c49432c885570720614d0b5d839ccfe37c
Sondq <sondq98@gmail.com>
2022-05-09 09:48:03 +0700

    push code api get big list and detail

--- nodejs_api/api/routes.js
+++ nodejs_api/api/routes.js
@@ -1,14 +1,27 @@
 'use strict';
 module.exports = function (app) {
-    let productsCtrl = require('./controllers/ProductsController');
+    // let productsCtrl = require('./controllers/ProductsController');
+
+    // // todoList Routes
+    // app.route('/products')
+    //     .get(productsCtrl.get)
+    //     .post(productsCtrl.store);
+
+    // app.route('/products/:productId')
+    //     .get(productsCtrl.detail)
+    //     .put(productsCtrl.update)
+    //     .delete(productsCtrl.delete);
+
+
+    let languageCtrl = require('./controllers/LanguageAppController');
 
     // todoList Routes
-    app.route('/products')
-        .get(productsCtrl.get)
-        .post(productsCtrl.store);
+    app.route('/categories')
+        .get(languageCtrl.get)
+        // .post(languageCtrl.store);
 
-    app.route('/products/:productId')
-        .get(productsCtrl.detail)
-        .put(productsCtrl.update)
-        .delete(productsCtrl.delete);
+    app.route('/categories/:categoryId')
+        .get(languageCtrl.detail)
+        // .put(languageCtrl.update)
+        // .delete(languageCtrl.delete);
 };
